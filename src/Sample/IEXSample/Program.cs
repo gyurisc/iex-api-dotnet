64612fc84b85ce7776e54006e3c0a17164d521fa
Krisztian Gyuris <gyurisc@gmail.com>
2018-10-16 09:23:59 +0200

    sample app

---
+++ src/Sample/IEXSample/Program.cs
@@ -0,0 +1,32 @@
+﻿using IEXAPI;
+using System;
+
+namespace IEXSample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string symbol = "msft";
+
+            var iex = new IEXClient();
+
+            var company = iex.GetCompany(symbol);
+            if (company != null)
+            {
+                Console.WriteLine($"{company.companyName} - ({company.symbol})");
+                Console.WriteLine($"CEO: {company.CEO}");
+                Console.WriteLine($"Website: {company.website}");
+            }
+
+            var quote = iex.GetStockQuote(symbol);
+            if (quote != null)
+            {
+                Console.WriteLine($"Price: {quote.LatestPrice:C}");           
+                Console.WriteLine($"Open-Close: {quote.Open:C}-{quote.Close:C}");
+                Console.WriteLine($"Change: {quote.Change} ({quote.ChangePercent:P})");
+                Console.WriteLine($"52 Week Range: {quote.Week52Low:C}-{quote.Week52High:C}");
+            }
+        }
+    }
+}
